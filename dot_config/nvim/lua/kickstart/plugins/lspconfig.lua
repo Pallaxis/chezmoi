35e02da23aca4321e271f6ad906bd3eb3fb716af
Pallaxis <sam_e3u@hotmail.com>
2025-03-11 11:38:54 +1300

    adding linter and lsp for python (hopefully)

--- dot_config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ dot_config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -184,7 +184,7 @@ return {
 			local servers = {
 				-- clangd = {},
 				-- gopls = {},
-				-- pyright = {},
+				--pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
@@ -227,6 +227,8 @@ return {
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"pylint",
+				"pyright",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
