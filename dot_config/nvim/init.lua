97cd0cb20f30e796149c1ee8ba191640d1a09ae8
Pallaxis <sam_e3u@hotmail.com>
2025-03-31 10:23:47 +1300

    working on getting tab settings right

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,13 +1,13 @@
 --  NOTE: Must set this before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
@@ -16,7 +16,7 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  vim.opt.clipboard = "unnamedplus"
 end)
 
 vim.opt.breakindent = true
@@ -25,7 +25,7 @@ vim.opt.undofile = true
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
@@ -35,10 +35,16 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Sets how tabs will be handled
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = -1
+vim.opt.expandtab = true
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
@@ -50,17 +56,17 @@ vim.opt.confirm = true
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- TODO: Not working, afaik it's supposed to remove the inline nags from linters and let you hover instead
-vim.diagnostic.config {
+vim.diagnostic.config({
   virtual_text = false,
   signs = true,
-  float = { border = 'single' },
-}
+  float = { border = "single" },
+})
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -68,7 +74,7 @@ vim.diagnostic.config {
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -80,10 +86,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- NOTE: Some terminals have coliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -92,9 +98,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
@@ -102,12 +108,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
+    error("Error cloning lazy.nvim:\n" .. out)
   end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
@@ -115,35 +121,35 @@ vim.opt.rtp:prepend(lazypath)
 -- [[ Configure and install plugins ]]
 -- NOTE: Here is where you install your plugins.
 
-require('lazy').setup({
+require("lazy").setup({
   -- Imports all plugins inside of custom/plugins/
-  { import = 'custom.plugins' },
+  { import = "custom.plugins" },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
+    ui = {
+      -- If you are using a Nerd Font: set icons to an empty table which will use the
+      -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+      icons = vim.g.have_nerd_font and {} or {
+        cmd = "⌘",
+        config = "🛠",
+        event = "📅",
+        ft = "📂",
+        init = "⚙",
+        keys = "🗝",
+        plugin = "🔌",
+        runtime = "💻",
+        require = "🌙",
+        source = "📄",
+        start = "🚀",
+        task = "📌",
+        lazy = "💤 ",
+      },
     },
-  },
-})
+  })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- scripts/bash/burnin/executable_download.sh
+++ scripts/bash/burnin/executable_download.sh
@@ -8,110 +8,110 @@ rvp_folders=("rvp-ats-ansible-platbook-rpi")
 download_folder="$HOME/media/videos/burn-in/"	# Where we want to save the output to
 host="mikyla"	# Set this to the burn-in test bench ( mikyla@10.71.0.125 for the ubuntu macbook upstairs )
 
-	help() {
-		printf -- "-h, --help	Show this help menu & exit\n"
-		printf -- "-s, --start	Create folder structure and download into start folder\n"
-		printf -- "-e, --end	IMPORTANT: Only run after teardown has been completed! Download to end folder of most recent project folder\n"
-		exit
-	}
+help() {
+    printf -- "-h, --help	Show this help menu & exit\n"
+    printf -- "-s, --start	Create folder structure and download into start folder\n"
+    printf -- "-e, --end	IMPORTANT: Only run after teardown has been completed! Download to end folder of most recent project folder\n"
+    exit
+}
 
-	start() {
-		printf "Currently selected rvp is: $rvp_folders\n"
-#		read -p "Have you saved all videos and dmesgs from last run?" yn
-#		case $yn in
-#			[yY] echo "Starting new run...";
-#			ssh mikyla;;
-#			[nN] echo "Exiting...";
-#			exit;;
-		cd $download_folder
-		folder_name=burn_in_$(date '+%d-%m-%y')
-		echo $folder_name
-		if [[ -d $folder_name ]]; then
-			echo "$folder_name already exists. Exiting..."
-			exit
-		else
-			echo "Creating folder: $folder_name"
-			mkdir $folder_name $folder_name/start $folder_name/end
+start() {
+    printf "Currently selected rvp is: $rvp_folders\n"
+    #		read -p "Have you saved all videos and dmesgs from last run?" yn
+    #		case $yn in
+    #			[yY] echo "Starting new run...";
+    #			ssh mikyla;;
+    #			[nN] echo "Exiting...";
+    #			exit;;
+    cd $download_folder
+    folder_name=burn_in_$(date '+%d-%m-%y')
+    echo $folder_name
+    if [[ -d $folder_name ]]; then
+        echo "$folder_name already exists. Exiting..."
+        exit
+    else
+        echo "Creating folder: $folder_name"
+        mkdir $folder_name $folder_name/start $folder_name/end
 
-			for remote_path in "${rvp_folders[@]}"; do
-				run_scp "$remote_path" "$folder_name/start" "start"
-				echo "ping"
-			done
-		fi
-	}
+        for remote_path in "${rvp_folders[@]}"; do
+            run_scp "$remote_path" "$folder_name/start" "start"
+            echo "ping"
+        done
+    fi
+}
 
 end() {
-	cd $download_folder
-	newest_file=$(ls -Art | tail -n 1)
-	echo $newest_file
-	# Bit of a mess of if statements, should clean up a bit
-	if [ -z "$newest_file" ]; then
-		echo "No folder exists, has the start command been run? Exiting..."
-		exit
-	else 
-		echo "Folder exists, can continue"
-	fi
-	if [ -z "$( ls -A "$newest_file/end" )" ]; then
-		echo "Folder exists and is ready to be downloaded to."
-	else
-		echo "end folder is not empty. Exiting..."
-		exit
-	fi
+    cd $download_folder
+    newest_file=$(ls -Art | tail -n 1)
+    echo $newest_file
+    # Bit of a mess of if statements, should clean up a bit
+    if [ -z "$newest_file" ]; then
+        echo "No folder exists, has the start command been run? Exiting..."
+        exit
+    else 
+        echo "Folder exists, can continue"
+    fi
+    if [ -z "$( ls -A "$newest_file/end" )" ]; then
+        echo "Folder exists and is ready to be downloaded to."
+    else
+        echo "end folder is not empty. Exiting..."
+        exit
+    fi
 
-	#	Running before teardown has completed will just give the same results as the start but in the end folder
-	echo "!!!MAKE SURE TEARDOWN HAS BEEN RUN!!!"
+        #	Running before teardown has completed will just give the same results as the start but in the end folder
+        echo "!!!MAKE SURE TEARDOWN HAS BEEN RUN!!!"
 
-	for remote_path in "${rvp_folders[@]}"; do
-		run_scp "$remote_path" "$newest_file/end" "end"
-	done
-echo "Files downloaded, exiting."
-exit
-}
+        for remote_path in "${rvp_folders[@]}"; do
+            run_scp "$remote_path" "$newest_file/end" "end"
+        done
+        echo "Files downloaded, exiting."
+        exit
+    }
 
-run_scp() {
-	local remote_path=$1
-	local file_destination=$2
-	local copy_mode=$3
+    run_scp() {
+        local remote_path=$1
+        local file_destination=$2
+        local copy_mode=$3
 
-	if [[ "$copy_mode" == "start" ]]; then
-		echo "start selected for scp"
-		scp "$host:$remote_path"/*.mp4 "$file_destination"
-	elif [[ "$copy_mode" == "end" ]]; then
-		echo "end selected for scp"
-		scp "$host:$remote_path"/*.mp4 "$file_destination"
-		scp "$host:$remote_path"/*.out "$file_destination"
-	else
-		# This should never trigger lol
-		echo "how did we get here?"
-	fi
-}
+        if [[ "$copy_mode" == "start" ]]; then
+            echo "start selected for scp"
+            scp "$host:$remote_path"/*.mp4 "$file_destination"
+        elif [[ "$copy_mode" == "end" ]]; then
+            echo "end selected for scp"
+            scp "$host:$remote_path"/*.mp4 "$file_destination"
+            scp "$host:$remote_path"/*.out "$file_destination"
+        else
+            # This should never trigger lol
+            echo "how did we get here?"
+        fi
+    }
 
-VALID_ARGS=$(getopt -o seh --long start,end,help -- "$@")
-if [[ $? -ne 0 ]]; then
-	exit 1;
-fi
+    VALID_ARGS=$(getopt -o seh --long start,end,help -- "$@")
+    if [[ $? -ne 0 ]]; then
+        exit 1;
+    fi
 
-eval set -- "$VALID_ARGS"
-while [ : ]; do
-	case "$1" in
-		-s | --start)
-			echo "Processing 'start' option"
-			start
-			shift
-			;;
-		-e | --end)
-			echo "Processing 'end' option"
-			end
-			shift
-			;;
-		-h | --help)
-			echo "Processing 'help' option"
-			help
-			shift
-			;;
-		--) shift; 
-			break 
-			;;
-	esac
+    eval set -- "$VALID_ARGS"
+    while [ : ]; do
+        case "$1" in
+            -s | --start)
+                echo "Processing 'start' option"
+                start
+                shift
+                ;;
+            -e | --end)
+                echo "Processing 'end' option"
+            end
+            shift
+            ;;
+        -h | --help)
+            echo "Processing 'help' option"
+            help
+            shift
+            ;;
+        --) shift; 
+            break 
+            ;;
+    esac
 done
-
+# vim: sw=4 et
