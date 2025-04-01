ef699afeb7dd0c9b9da5f97ce12ef6a2676dbc94
Pallaxis <sam_e3u@hotmail.com>
2025-04-01 16:27:08 +1300

    auto opens neo tree on dir, alias for ffplay and ffplay script

--- dot_config/nvim/lua/custom/plugins/neo-tree.lua
+++ dot_config/nvim/lua/custom/plugins/neo-tree.lua
@@ -13,6 +13,15 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
+    init = function()
+    vim.g.neo_tree_remove_legacy_commands = 1
+    if vim.fn.argc() == 1 then
+      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == "directory" then
+        require("neo-tree")
+      end
+    end
+  end,
   opts = {
     filesystem = {
       window = {
