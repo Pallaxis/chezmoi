f41edf7c0af4bec4364c4020d2b6134f891e9686
Pallaxis <sam_e3u@hotmail.com>
2025-04-23 09:11:20 +1200

    less flashy hyprland animations, C-v prefix for tmux, adding some useful executables

--- dot_local/share/bin/executable_md-notes.sh
+++ dot_local/share/bin/executable_md-notes.sh
@@ -6,14 +6,14 @@ TERMINAL=alacritty
 
 newnote () { \
     name="$(echo "" | rofi -dmenu -p "Enter a name: " <&-)"
-    setsid -f "$TERMINAL" -e nvim $notesfolder$name".txt" &> /dev/null
+    setsid -f "$TERMINAL" -e nvim $notesfolder$name".md" &> /dev/null
 }
 
 selected () { \
-    choice=$(echo -e "New\n$(ls -t1 $notesfolder | grep '.txt')" | rofi -dmenu -p "Choose note or create new: ")
+    choice=$(echo -e "New\n$(ls -t1 $notesfolder | grep '.md')" | rofi -dmenu -p "Choose note or create new: ")
     case $choice in
         New) newnote;;
-        *txt) setsid -f "$TERMINAL" -e nvim "$notesfolder$choice" &> /dev/null;;
+        *md) setsid -f "$TERMINAL" -e nvim "$notesfolder$choice" &> /dev/null;;
         *) exit;;
     esac
 }
@@ -22,4 +22,4 @@ main() {
     setsid -f "$TERMINAL" -e nvim $notesfolder
 }
 
-main
+selected
