b755b6eca7346fa74c42f81f3c1eb23b2edbe6bf
Pallaxis <sam_e3u@hotmail.com>
2025-06-12 16:48:36 +1200

    greatest setting ever inside hyprland.conf

--- dot_config/tmux/scripts/executable_new-session.sh
+++ dot_config/tmux/scripts/executable_new-session.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 
-name="$(echo "" | rofi -dmenu -p "Enter session name: " <&-)"
+#name="$(echo "" | rofi -dmenu -p "Enter session name: " <&-)"
+#
+#if [[ -n $TMUX ]]; then
+#    tmux new-session -Ad -s $name
+#    tmux switch -t $name
+#fi
 
-if [[ -n $TMUX ]]; then
-    tmux new-session -Ad -s $name
-    tmux switch -t $name
+
+
+
+# Get the list of tmuxp sessions
+sessions=$(tmuxp ls 2>/dev/null)
+
+# Use rofi to prompt the user
+chosen=$(echo "$sessions" | rofi -dmenu -i -p "Tmuxp session or new name:")
+
+# Exit if no selection
+[ -z "$chosen" ] && exit
+
+# If the chosen input matches an existing tmuxp session
+if echo "$sessions" | grep -qx "$chosen"; then
+    # Load the tmuxp session
+    tmuxp load "$chosen" --yes > /dev/null 2>&1
+else
+    # Create a new tmux session with that name
+    tmux new-session -A -s "$chosen"
 fi
